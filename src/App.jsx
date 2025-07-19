bf7b9f41580257308d1d9982ebe58bbe64d6f693
dgupta52 <dgupta52@wisc.edu>
2025-07-19 00:13:37 -0500

    third trial

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 // src/App.jsx
 import React from 'react';
-import Navbar from './components/navbar';
-import Home from './components/home';
-// import other components as you create them
-// import About from './components/About';
-// import Skills from './components/Skills';
-// etc.
+import Navbar from './components/Navbar'; // <-- Capital 'N'
+import Home from './components/Home';     // <-- Capital 'H'
 
 function App() {
   return (
-    <div className="background">
+    <div> {/* Use a simple div, not a class 'background' unless you define it in CSS */}
       <Navbar />
       <main>
         <section id="home">
           <Home />
         </section>
-        
-        {/*
-        <section id="about">
-          <About />
-        </section>
 
-        <section id="skills">
-          <Skills />
-        </section>
-        
-        ... and so on for each section
-        */}
+        {/* ... other sections will go here ... */}
       </main>
     </div>
   );
