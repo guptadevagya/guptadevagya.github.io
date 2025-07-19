fd218e0da0362b57d69495d0d913a698ce803131
dgupta52 <dgupta52@wisc.edu>
2025-07-18 23:59:50 -0500

    first trial

---
+++ src/components/home.jsx
@@ -0,0 +1,24 @@
+// create this for the top section of your page
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container-fluid p-3 p-md-5 text-center d-flex align-items-center justify-content-center home">
+      <div className="col-xl-auto mx-auto">
+        <h1 className="display-3 fw-bold text-white">Devagya Gupta</h1>
+        <h3 className="fw-normal text-warning mb-3">Computer Science @ University of Wisconsin - Madison '26</h3>
+        <div className='container d-flex justify-content-center mb-4'>
+          <a className="nes-btn is-error mx-3" href="https://www.linkedin.com/in/devagyagupta/" target="_blank" rel="noopener noreferrer">
+            <i className="nes-icon linkedin is-medium"></i>
+          </a>
+          <a className="nes-btn is-success mx-3" href="https://github.com/guptadevagya" target="_blank" rel="noopener noreferrer">
+            <i className="nes-icon github is-medium"></i>
+          </a>
+        </div>
+        <a className="nes-btn is-warning" href="/assets/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item"><a className="nav-link" href="#about">About Me</a></li>
+            <li className="nav-item"><a className="nav-link" href="#skills">Skills</a></li>
+            <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
+            <li className="nav-item"><a className="nav-link" href="#resume">Resume</a></li>
+            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
