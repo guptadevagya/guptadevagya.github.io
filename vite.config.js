8e55f8d1977efaeb088fa43e0f78ba72f5cfd754
dgupta52 <dgupta52@wisc.edu>
2025-07-18 23:49:33 -0500

    set up pt.2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this to a single forward slash
-  build: {
-    outDir: 'docs',
-  },
-});
+})
