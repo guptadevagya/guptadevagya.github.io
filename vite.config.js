e110eed234f06bbeb9c54171ba707e6c0dceec9f
dgupta52 <dgupta52@wisc.edu>
2025-07-18 23:43:59 -0500

    trial 1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Change this to a single forward slash
   build: {
     outDir: 'docs',
   },
