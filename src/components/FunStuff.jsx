3d8934fdadaaf5e745f535fb3883484243f01ff3
dgupta52 <dgupta52@wisc.edu>
2025-07-21 16:46:56 -0500

    fix: trying to add a carousel for fun

--- src/components/FunStuff.jsx
+++ src/components/FunStuff.jsx
@@ -1,70 +1,77 @@
-import React, { useEffect } from 'react';
-import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-// add these three lines to import swiper's core styles
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+// Import your custom styles
 import styles from './FunStuff.module.css';
+
+// import required modules
 import { Navigation, Pagination } from 'swiper/modules';
 
 function FunStuff() {
-  useEffect(() => {
-    new Swiper(`.${styles.card__content}`, {
-      modules: [Navigation, Pagination],
-      loop: true,
-      spaceBetween: 32,
-      grabCursor: true,
-      pagination: {
-        el: '.swiper-pagination', // Use global class for pagination
-        clickable: true,
-        dynamicBullets: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next', // Use global class for navigation
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints:{
-        600: { slidesPerView: 2 },
-        968: { slidesPerView: 3 },
-      },
-    });
-  }, []);
+  const funActivities = [
+    {
+      name: 'Activity One',
+      description: 'A short description of the fun activity I do.',
+      images: ['/assets/fun-stuff/activity1/1.jpeg', '/assets/fun-stuff/activity1/2.jpeg', '/assets/fun-stuff/activity1/3.jpeg']
+    },
+    {
+      name: 'Activity Two',
+      description: 'This is another fun thing that I enjoy.',
+      images: ['/assets/fun-stuff/activity2/1.jpeg', '/assets/fun-stuff/activity2/2.jpeg', '/assets/fun-stuff/activity2/3.jpeg']
+    },
+    {
+      name: 'Activity Three',
+      description: 'And here is a third hobby or interest.',
+      images: ['/assets/fun-stuff/activity3/1.jpeg', '/assets/fun-stuff/activity3/2.jpeg', '/assets/fun-stuff/activity3/3.jpeg']
+    }
+  ];
 
   return (
     <section id="fun" className={styles.container}>
       <h3 className="text-center text-white my-5">Fun Stuff</h3>
-      <div className={styles.card__container}>
-        <div className={`${styles.card__content} swiper`}>
-          <div className="swiper-wrapper">
-            <article className={`${styles.card__article} swiper-slide`}>
-              <div className={styles.card__image}>
-                <img src="/assets/fun-stuff/activity1.jpeg" alt="Activity 1" className={styles.card__img} />
-                <div className={styles.card__shadow}></div>
-              </div>
-              <div className={styles.card__data}>
-                <h3 className={styles.card__name}>Activity One</h3>
-                <p className={styles.card__description}>A short description of the fun activity I do.</p>
-              </div>
-            </article>
-            <article className={`${styles.card__article} swiper-slide`}>
-              <div className={styles.card__image}>
-                <img src="/assets/fun-stuff/activity2.jpeg" alt="Activity 2" className={styles.card__img} />
-                <div className={styles.card__shadow}></div>
-              </div>
-              <div className={styles.card__data}>
-                <h3 className={styles.card__name}>Activity Two</h3>
-                <p className={styles.card__description}>This is another fun thing that I enjoy.</p>
-              </div>
-            </article>
-          </div>
-        </div>
-        
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-        <div className="swiper-pagination"></div>
-      </div>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        loop={true}
+        spaceBetween={32}
+        grabCursor={true}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        breakpoints={{
+          600: { slidesPerView: 2 },
+          968: { slidesPerView: 3 },
+        }}
+        className={styles.card__content}
+      >
+        {funActivities.map((activity) => (
+          <SwiperSlide key={activity.name} className={styles.card__article}>
+            <div className={styles.card__image}>
+              <Swiper
+                modules={[Navigation]}
+                navigation
+                className={styles.nested_swiper}
+              >
+                {activity.images.map((image, imgIndex) => (
+                  <SwiperSlide key={imgIndex}>
+                    <img src={image} alt={`${activity.name} ${imgIndex + 1}`} className={styles.card__img} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+              <div className={styles.card__shadow}></div>
+            </div>
+            <div className={styles.card__data}>
+              <h3 className={styles.card__name}>{activity.name}</h3>
+              <p className={styles.card__description}>{activity.description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
