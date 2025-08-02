f47c956907dce7959c5c508cb0b4f00c3378fd32
dgupta52 <dgupta52@wisc.edu>
2025-08-01 21:55:42 -0500

    adding first 2 project tiles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,61 @@
+import React from 'react';
+import Section from './Section';
+import styles from './Projects.module.css';
+
 function Projects() {
+  const projectData = [
+    {
+      name: 'waffle-game',
+      description:
+        'an addicting, waffle-shaped word puzzle. unscramble six intersecting words before you run out of swaps.',
+      image: '/assets/projects/waffle.png',
+      githubLink: 'https://github.com/guptadevagya/waffle-game',
+    },
+    {
+      name: 'weather-data-service',
+      description:
+        'my take on a fault-tolerant weather service. it’s fast, scalable, and built with a smarter data schema so it never quits.',
+      image: '/assets/projects/weather.png',
+      githubLink: 'https://github.com/guptadevagya/weather-data-service',
+    },
+  ];
+
   return (
-    <section id="projects" className="container text-center">
-      <h3 className="text-white my-5">Projects</h3>
-      {/* Add your project cards here */}
-      <p>My projects will be listed here soon!</p>
-    </section>
+    <Section id="projects" title="Projects">
+      <div className="container" style={{ maxWidth: '1200px' }}>
+        <div className="row justify-content-center">
+          {projectData.map((project, index) => (
+            // change col-lg-6 to col-lg-5 to make the cards smaller
+            <div key={index} className="col-lg-5 mb-5">
+              <div className={styles.card__article}>
+                <div className={styles.card__image}>
+                  <img
+                    src={project.image}
+                    alt={project.name}
+                    className={styles.card__img}
+                  />
+                  <a
+                    href={project.githubLink}
+                    className={styles.githubIcon}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="nes-icon github is-medium"></i>
+                  </a>
+                  <div className={styles.card__shadow}></div>
+                </div>
+                <div className={styles.card__data}>
+                  <h3 className={styles.card__name}>{project.name}</h3>
+                  <p className={styles.card__description}>
+                    {project.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
   );
 }
 
